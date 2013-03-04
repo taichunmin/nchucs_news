e003b4b821f6e9f7c516e884e0784b654efc62c4
taichunmin <taichunmin@gmail.com>
2013-03-04 11:25:00 +0800

    新增移機安裝說明

---
+++ 安裝簡易說明.php
@@ -0,0 +1,30 @@
+IP：140.120.15.141
+
+系統帳密：Administrator、dmlab702b715
+
+安裝 Appserv 2.6.0 (PHP伺服器) 於 D:\Appserv
+http://www.appservnetwork.com/
+
+MySQL 帳密：root、dmlab702b
+
+安裝 Teamviewer (遠端)
+
+修改 C:\Windows\System32\drivers\etc\hosts
+複製到桌面，然後修改完成之後，再貼上覆蓋
+
+安裝 phpMyAdmin，並修改設定檔
+
+安裝 Notepad++
+
+安裝 Everything
+
+將 程式碼 放置 根目錄資料夾
+例：D:\Appserv\wwwhtml\
+
+匯入 database_structure.sql
+
+設定排程 exec_daily.bat
+
+手動執行一次 exec_daily.bat 確認是否正常執行
+
+查看網站是否正常執行
