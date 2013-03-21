658d122355b21e25f35bd90913de04210027ffe0
高蔓蔓 <cobepunpun@gmail.com>
2013-03-21 20:54:17 +0800

    加上註解

--- ontology.php
+++ ontology.php
@@ -1,6 +1,9 @@
 <?php
-	include_once('include.php');
+	include_once('include.php');//資料庫連接
+	
 	// uid => array( count_nid => cid => n , tf_list => cid => term => freq )
+	//使用者資料:(看過新聞數=>分種類=>分類底下的新聞數，
+	//            看過新聞的關鍵字=>類別=>關建字=>出現次數)  
 	function rc4getData()
 	{
 		$d = array();
@@ -34,6 +37,7 @@
 		return $d;
 	}
 	// cid => nid => tid => cnt
+	//類別下新聞所用關鍵字的出現次數
 	function getNewNewsCateTerm($date = null)
 	{
 		
@@ -71,42 +75,42 @@
 		return $new;
 	}
 	
-	function ontology($date = null)
+	function ontology($date = null)//比對瀏覽歷史跟個人本體興趣，找出適合推薦的新聞
 	{
-		$data = rc4getData();
-		$new = getNewNewsCateTerm($date);
+		$data = rc4getData();//使用者資料
+		$new = getNewNewsCateTerm($date);//新的新聞的分類跟關鍵字
 		
 		//tai_vardebug($new);
 		//資料preset
-		$classSelect=41;
-		$termSelect=0;
-		$result = array();
+		$classSelect=41;//新聞分類預設要取全部41種類分類(可以讓使用者自己選擇要取前N樣)
+		$termSelect=0;//新聞的關鍵字(我們自己斷詞出來的)
+		$result = array();//推薦結果
 		/*演算法開始*/
-		foreach($data as $uid => $cid_list){
-			/*Step1：新建一個使用者CLASS的權重表=>加總選定的CLASS的新聞總數=>標準化*/
-			arsort($cid_list['count_nid']);//如果資料抓出來就有遞減排序，可刪
-			if($classSelect>count($cid_list)){
+		foreach($data as $uid => $cid_list){//使用者擁有的分類
+			/*Step1：新建一個使用者CLASS的權重表=>加總選定的CLASS的新聞總數排序=>標準化*/
+			arsort($cid_list['count_nid']);//如果資料抓出來就有遞減排序，(如果資料進來已經是排好序的，這行就可刪)
+			if($classSelect>count($cid_list)){//如果使用者自己設定的分類總數>自己看過的分類種數，那只給使用者分類總數
 				$cid_weight_list=$cid_list['count_nid'];
 			}
-			else{
+			else{//
 				$cid_weight_list=array_slice($cid_list['count_nid'],0,$classSelect,true);
 			}
 			//tai_vardebug($cid_weight_list);//測試輸出使用者排名前$classSelect的類別
-			$sum_nid=array_sum($cid_weight_list);
-			$cid_tid_weight=array();
-			foreach($cid_weight_list as $cid => $weight){
-				$cid_weight_list[$cid]/=$sum_nid;
+			$sum_nid=array_sum($cid_weight_list);//選定新聞類別的新聞總數全部新聞的數目
+			$cid_tid_weight=array();//用來存放選定類別底下新聞所含的關鍵字出現的次數
+			foreach($cid_weight_list as $cid => $weight){//選定類別下有幾篇新聞
+				$cid_weight_list[$cid]/=$sum_nid;//算出各個類別所佔的百分比
 				/*Step2：新建term的權重表=>加總CLASS下的freq總數=>標準化*/
 				arsort($cid_list['tf_list'][$cid]);//如果資料抓出來就有遞減排序，可刪
 				//tai_vardebug($cid_list['tf_list'][$cid]);
-				if($termSelect<=0 || $termSelect>count($cid_list['tf_list'][$cid])){
+				if($termSelect<=0 || $termSelect>count($cid_list['tf_list'][$cid])){//如果使用者設定的關鍵字總數比自己看過得還多
 					$cid_tid_weight[$cid]=$cid_list['tf_list'][$cid];
 				}
 				else{
 					$cid_tid_weight[$cid]=array_slice($cid_list['tf_list'][$cid],0,$termSelect,true);
 				}
 				//tai_vardebug($cid_tid_weight[$cid]);//測試輸出使用者類別下排名前$termSelect的詞
-				$sum_freq=array_sum($cid_tid_weight[$cid]);
+				$sum_freq=array_sum($cid_tid_weight[$cid]);//所有關鍵字出現次數加起來
 				foreach($cid_tid_weight[$cid] as $tid => $freq){
 					$cid_tid_weight[$cid][$tid]/=$sum_freq;//兩項表不先相乘，等HIT在乘，盡可能減少運算空間
 				}//跳出=>該類的詞權重表完成
@@ -114,12 +118,13 @@
 				tai_vardebug($cid_tid_weight[$cid]);
 				echo '<br />'; /**/
 				/*Step：比較新進新聞*/
-				if(is_array($new[$cid]))
-					foreach($new[$cid] as $nid =>$tf_list){
-						if($intersect=array_intersect_key($tf_list,$cid_tid_weight[$cid])){
-							/*tai_vardebug($intersect);
+				if(is_array($new[$cid]))//取得新進新聞的類別
+					foreach($new[$cid] as $nid =>$tf_list){//類別下的其中一篇新進新聞跟每個新聞所用的關鍵字的頻率
+						if($intersect=array_intersect_key($tf_list,$cid_tid_weight[$cid])){//如果新進新聞的關鍵字也有出現在使用者選擇的關鍵字才往下走
+							/*tai_vardebug($intersect);                                    //這個新進新聞關鍵字跟他的數量和使用者在這個類別下看過新聞的關鍵字頻率加總
+							                                                               //                               (只在乎類別下使用者看過的關鍵字，不在乎是來自甚麼新聞)
 							echo '<br />'; /**/
-							foreach($intersect as $tid =>$freq){
+							foreach($intersect as $tid =>$freq){//最後推薦評分加總(交集中出現的關鍵字(新進新聞中的關鍵字的頻率)*關鍵字在使用者類別之下出現的比例*類別對於使用者的比例)
 								$result[$uid][$nid]+=$freq*$cid_tid_weight[$cid][$tid]*$cid_weight_list[$cid];
 							}
 						}
--- similarity.php
+++ similarity.php
@@ -21,54 +21,57 @@
 		if( count($data[$uidRow['uid']])==0 ) unset($data[$uidRow['uid']]);
 	}
 	@mysql_free_result($uidRes);
-	$user_termFreq=array();//存更號平方合
+	
+	$user_termFreq=array();//存根號平方合 Cosine similarity分母(兩個使用者)
 	$userList=array();//為了要以index定位，看能不能省去
-	foreach($data as $uid => $arr_cid){
+	foreach($data as $uid => $arr_cid){//使用者看過新聞的所有類別
 		$merge_termFreq=array();//將class的term&frequency合併
 		$user=$arr_cid;
-		$userList[]=$uid;
-		foreach($user as $cid => $arr_termFreq){
-			$termList=$arr_termFreq;
-			foreach($termList as $term =>$frequency){
+		$userList[]=$uid;//記錄算了幾個USER
+		foreach($user as $cid => $arr_termFreq){//使用者類別下關鍵字及其頻率
+			$termList=$arr_termFreq;//記錄使用者用過的關鍵字和頻率
+			foreach($termList as $term =>$frequency){//針對每個關鍵字和頻率計算
+			    //
 				$merge_termFreq=array_pad($merge_termFreq,count($merge_termFreq)+$frequency,$term);//重覆append
 			}
 		}
-		$merge_termFreq=array_count_values($merge_termFreq);//計算次數
-		foreach($merge_termFreq as $term => $frequency){
+		$merge_termFreq=array_count_values($merge_termFreq);//計算次數 關鍵字和其出現次數
+		foreach($merge_termFreq as $term => $frequency){//平方相加(一個使用者)
 			$temp+=$frequency*$frequency;
 		}
-		$temp=sqrt($temp);
-		$user_termFreq[$uid] =$temp;
+		$temp=sqrt($temp);//開根號
+		$user_termFreq[$uid] =$temp;//算完這個USER的值，存進去
 		//echo $temp.'<br />';
 		$temp=0;
 	}
-	foreach($userList as $index => $uid){
+	foreach($userList as $index => $uid){//算所有使用者間的相似度
 		$arr_cid=$data[$uid];//第一人
 		while($arr_cid2=$data[$userList[++$index]]){//第二人
 			//Step:兩人對class的關係表
 			//print_r(array_keys($arr_cid));
 			//print_r(array_keys($arr_cid2));
 			//echo '<br />';
-			$merge_cid=array_merge(array_keys($arr_cid),array_keys($arr_cid2));
+			$merge_cid=array_merge(array_keys($arr_cid),array_keys($arr_cid2));//
 			sort($merge_cid);
 			$merge_cid=array_count_values($merge_cid);
 			foreach($merge_cid as $cid => $case){
 				if($case==2){//CLASS中兩人對term的關係表
-					$merge_tid=array_merge(array_keys($arr_cid[$cid]),array_keys($arr_cid2[$cid]));
-					sort($merge_tid);
-					$merge_tid=array_count_values($merge_tid);
-					foreach($merge_tid as $tid => $case){
+					$merge_tid=array_merge(array_keys($arr_cid[$cid]),array_keys($arr_cid2[$cid]));//array_keys():取ARRAY的INDEX值;array_merge():UNION 兩個ARRAY
+					                                                                               
+					sort($merge_tid);//排序兩個使用者用過的關鍵字
+					$merge_tid=array_count_values($merge_tid);//統計每個關鍵字出現的次數，由此知道兩個使用者交集的關鍵字($tid =>1 OR 2)
+					foreach($merge_tid as $tid => $case){//如果"$tid =>2，表示該關鍵字為兩人皆使用
 						if($case==2){
-							$similarity+=$arr_cid[$cid][$tid]*$arr_cid2[$cid][$tid];
+							$similarity+=$arr_cid[$cid][$tid]*$arr_cid2[$cid][$tid];//Cosine similarity分子(兩個使用者相同類別下相同關建字)
 						}
 					}
 				}
 			}
-			if($user_termFreq[$uid]*$user_termFreq[$userList[$index]]!=0){
+			if($user_termFreq[$uid]*$user_termFreq[$userList[$index]]!=0){//分母中第一個人和第二個人相乘不是等於0，才往下算
 				$similarity/=$user_termFreq[$uid]*$user_termFreq[$userList[$index]];
 			}
 			//echo 'uid='.$uid.'跟uid='.$userList[$index].'的同好度為:'.$similarity.'<br />';
-			$simi->set($uid, $userList[$index], $similarity);
+			$simi->set($uid, $userList[$index], $similarity);//A對B相似度多少計算完後存入SET中
 			$similarity=0;
 		}
 		
