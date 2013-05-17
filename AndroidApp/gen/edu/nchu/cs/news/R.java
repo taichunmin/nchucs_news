cf01a12e5fad68e08dacf7a17ae6e8f87d75363d
taichunmin <taichunmin@gmail.com>
2013-05-17 08:13:48 +0800

    新增許多背景漸層顏色

--- AndroidApp/gen/edu/nchu/cs/news/R.java
+++ AndroidApp/gen/edu/nchu/cs/news/R.java
@@ -24,10 +24,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int background_black=0x7f020000;
-        public static final int background_blue=0x7f020001;
-        public static final int background_white=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int play_icon=0x7f020004;
+        public static final int background_blackred=0x7f020001;
+        public static final int background_blue=0x7f020002;
+        public static final int background_green=0x7f020003;
+        public static final int background_lightblue=0x7f020004;
+        public static final int background_orange=0x7f020005;
+        public static final int background_red=0x7f020006;
+        public static final int background_white=0x7f020007;
+        public static final int background_yellow=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int play_icon=0x7f02000a;
     }
     public static final class id {
         public static final int action_forgot_password=0x7f090015;
